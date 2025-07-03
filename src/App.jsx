40d0cf0667bf3ca3f442a23c6333cad812ddd38f
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-07-03 20:12:37 -0300

    finalizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import senacLogo from './assets/senac.webp'
+
 import './App.css'
 
 function App() {
@@ -9,24 +10,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      
+        <a href="https://sp.senac.br/americana" target="_blank">
+          <img src={senacLogo} className="logo react" alt="Senac Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Começando com React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador é {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Clique na imagem e conheça nossos cursos!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       💧 Beba água 💧
       </p>
     </>
   )
